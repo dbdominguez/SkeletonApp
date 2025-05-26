96c8c5e548c9f995aded4a7c2c85b1fd4422abed
dbdominguez <d.dominguezg@duocuc.cl>
2025-05-25 23:26:05 -0400

    Conectar home con login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: false,
 })
 export class HomePage {
+  constructor(private router: Router) {}
 
-  constructor() {}
-
+  irAlLogin() {
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonModal,
+  IonTitle,
+  IonToolbar,
+  IonFooter, 
+} from '@ionic/angular/standalone';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  imports: [
+    FormsModule,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonInput,
+    IonItem,
+    IonModal,
+    IonTitle,
+    IonToolbar,
+    IonFooter
+  ],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent {
+  @ViewChild(IonModal) modal!: IonModal;
 
-  constructor() { }
+  message = 'Inicio de sesión de prueba';
+  name!: string;
+  password: string = '';
 
-  ngOnInit() {}
+  constructor(private router: Router) {} 
 
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+    if (event.detail.role === 'confirm') {
+      this.message = `Bienvenido, ${event.detail.data}!`;
+    }
+  }
+
+  irAlHome() {
+    this.router.navigate(['/home']);
+  }
 }
